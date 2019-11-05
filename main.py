7f503a62f9a93c2ca081df2bee374cfe3b6ac87f
Linda Penner <v.a.l.penner@gmail.com>
2019-11-04 19:35:26 -0600

    added variables

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
+import datetime
 
-app = Flask()
-
+app = Flask(__name__)
+year = datetime.datetime.now().year
+name = "Linda Penner"
+cities = ["Edmonton", "Winnipeg", "Toronto"]
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", year=year, name=name)
+
+@app.route('/about-me')
+def about():
+    return render_template("about.html",cities=cities)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
+
